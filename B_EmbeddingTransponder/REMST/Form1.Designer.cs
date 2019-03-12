42b066d5233e607a4c647680792a2b7a47357735
Hanif Hamidi <Hanif@Melayu.org.uk>
2019-03-12 14:05:48 +0000

    Directory update

---
+++ B_EmbeddingTransponder/REMST/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace REMST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.btnOpen3 = new System.Windows.Forms.Button();
+            this.btnOpen4 = new System.Windows.Forms.Button();
+            this.btnOpen5 = new System.Windows.Forms.Button();
+            this.btnOpen9 = new System.Windows.Forms.Button();
+            this.btnOpen8 = new System.Windows.Forms.Button();
+            this.btnOpen7 = new System.Windows.Forms.Button();
+            this.btnOpen6 = new System.Windows.Forms.Button();
+            this.btnOpen10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen1.Location = new System.Drawing.Point(24, 30);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen1.TabIndex = 0;
+            this.btnOpen1.Text = "Mercury";
+            this.btnOpen1.UseVisualStyleBackColor = false;
+            this.btnOpen1.Click += new System.EventHandler(this.BtnOpen1_Click);
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen2.Location = new System.Drawing.Point(24, 78);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen2.TabIndex = 1;
+            this.btnOpen2.Text = "Venus";
+            this.btnOpen2.UseVisualStyleBackColor = false;
+            this.btnOpen2.Click += new System.EventHandler(this.BtnOpen2_Click);
+            // 
+            // btnOpen3
+            // 
+            this.btnOpen3.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen3.Location = new System.Drawing.Point(24, 126);
+            this.btnOpen3.Name = "btnOpen3";
+            this.btnOpen3.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen3.TabIndex = 2;
+            this.btnOpen3.Text = "Titan";
+            this.btnOpen3.UseVisualStyleBackColor = false;
+            this.btnOpen3.Click += new System.EventHandler(this.BtnOpen3_Click);
+            // 
+            // btnOpen4
+            // 
+            this.btnOpen4.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen4.Location = new System.Drawing.Point(24, 174);
+            this.btnOpen4.Name = "btnOpen4";
+            this.btnOpen4.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen4.TabIndex = 3;
+            this.btnOpen4.Text = "Io";
+            this.btnOpen4.UseVisualStyleBackColor = false;
+            this.btnOpen4.Click += new System.EventHandler(this.BtnOpen4_Click);
+            // 
+            // btnOpen5
+            // 
+            this.btnOpen5.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen5.Location = new System.Drawing.Point(24, 222);
+            this.btnOpen5.Name = "btnOpen5";
+            this.btnOpen5.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen5.TabIndex = 4;
+            this.btnOpen5.Text = "Jupiter";
+            this.btnOpen5.UseVisualStyleBackColor = false;
+            this.btnOpen5.Click += new System.EventHandler(this.BtnOpen5_Click);
+            // 
+            // btnOpen9
+            // 
+            this.btnOpen9.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen9.Location = new System.Drawing.Point(252, 174);
+            this.btnOpen9.Name = "btnOpen9";
+            this.btnOpen9.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen9.TabIndex = 8;
+            this.btnOpen9.Text = "WhatUniform";
+            this.btnOpen9.UseVisualStyleBackColor = false;
+            this.btnOpen9.Click += new System.EventHandler(this.BtnOpen9_Click);
+            // 
+            // btnOpen8
+            // 
+            this.btnOpen8.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen8.Location = new System.Drawing.Point(252, 126);
+            this.btnOpen8.Name = "btnOpen8";
+            this.btnOpen8.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen8.TabIndex = 7;
+            this.btnOpen8.Text = "HOAXHPhenomena";
+            this.btnOpen8.UseVisualStyleBackColor = false;
+            this.btnOpen8.Click += new System.EventHandler(this.BtnOpen8_Click);
+            // 
+            // btnOpen7
+            // 
+            this.btnOpen7.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen7.Location = new System.Drawing.Point(252, 78);
+            this.btnOpen7.Name = "btnOpen7";
+            this.btnOpen7.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen7.TabIndex = 6;
+            this.btnOpen7.Text = "Cshuffle_FrontendBackend";
+            this.btnOpen7.UseVisualStyleBackColor = false;
+            this.btnOpen7.Click += new System.EventHandler(this.BtnOpen7_Click);
+            // 
+            // btnOpen6
+            // 
+            this.btnOpen6.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen6.Location = new System.Drawing.Point(252, 30);
+            this.btnOpen6.Name = "btnOpen6";
+            this.btnOpen6.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen6.TabIndex = 5;
+            this.btnOpen6.Text = "RMND";
+            this.btnOpen6.UseVisualStyleBackColor = false;
+            this.btnOpen6.Click += new System.EventHandler(this.BtnOpen6_Click);
+            // 
+            // btnOpen10
+            // 
+            this.btnOpen10.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen10.Location = new System.Drawing.Point(252, 222);
+            this.btnOpen10.Name = "btnOpen10";
+            this.btnOpen10.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen10.TabIndex = 9;
+            this.btnOpen10.Text = "PreoccupancyManagementPlan";
+            this.btnOpen10.UseVisualStyleBackColor = false;
+            this.btnOpen10.Click += new System.EventHandler(this.BtnOpen10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 305);
+            this.Controls.Add(this.btnOpen10);
+            this.Controls.Add(this.btnOpen9);
+            this.Controls.Add(this.btnOpen8);
+            this.Controls.Add(this.btnOpen7);
+            this.Controls.Add(this.btnOpen6);
+            this.Controls.Add(this.btnOpen5);
+            this.Controls.Add(this.btnOpen4);
+            this.Controls.Add(this.btnOpen3);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnOpen1);
+            this.Name = "Form1";
+            this.Text = "REMST";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen1;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.Windows.Forms.Button btnOpen3;
+        private System.Windows.Forms.Button btnOpen4;
+        private System.Windows.Forms.Button btnOpen5;
+        private System.Windows.Forms.Button btnOpen9;
+        private System.Windows.Forms.Button btnOpen8;
+        private System.Windows.Forms.Button btnOpen7;
+        private System.Windows.Forms.Button btnOpen6;
+        private System.Windows.Forms.Button btnOpen10;
+    }
+}
+
---
+++ B_EmbeddingTransponder/REMST/fSub6.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace REMST
+{
+    partial class fSub6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen8 = new System.Windows.Forms.Button();
+            this.btnOpen7 = new System.Windows.Forms.Button();
+            this.btnOpen6 = new System.Windows.Forms.Button();
+            this.btnOpen5 = new System.Windows.Forms.Button();
+            this.btnOpen4 = new System.Windows.Forms.Button();
+            this.btnOpen3 = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen8
+            // 
+            this.btnOpen8.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen8.Location = new System.Drawing.Point(273, 193);
+            this.btnOpen8.Name = "btnOpen8";
+            this.btnOpen8.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen8.TabIndex = 16;
+            this.btnOpen8.Text = "Rummy";
+            this.btnOpen8.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen7
+            // 
+            this.btnOpen7.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen7.Location = new System.Drawing.Point(273, 145);
+            this.btnOpen7.Name = "btnOpen7";
+            this.btnOpen7.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen7.TabIndex = 15;
+            this.btnOpen7.Text = "PrintingTowers";
+            this.btnOpen7.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen6
+            // 
+            this.btnOpen6.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen6.Location = new System.Drawing.Point(273, 97);
+            this.btnOpen6.Name = "btnOpen6";
+            this.btnOpen6.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen6.TabIndex = 14;
+            this.btnOpen6.Text = "HanoiGenre";
+            this.btnOpen6.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen5
+            // 
+            this.btnOpen5.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen5.Location = new System.Drawing.Point(273, 49);
+            this.btnOpen5.Name = "btnOpen5";
+            this.btnOpen5.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen5.TabIndex = 13;
+            this.btnOpen5.Text = "Sudoku";
+            this.btnOpen5.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen4
+            // 
+            this.btnOpen4.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen4.Location = new System.Drawing.Point(44, 193);
+            this.btnOpen4.Name = "btnOpen4";
+            this.btnOpen4.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen4.TabIndex = 12;
+            this.btnOpen4.Text = "Dominoes";
+            this.btnOpen4.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen3
+            // 
+            this.btnOpen3.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen3.Location = new System.Drawing.Point(44, 145);
+            this.btnOpen3.Name = "btnOpen3";
+            this.btnOpen3.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen3.TabIndex = 11;
+            this.btnOpen3.Text = "SIMPL";
+            this.btnOpen3.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen2.Location = new System.Drawing.Point(44, 97);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen2.TabIndex = 10;
+            this.btnOpen2.Text = "743";
+            this.btnOpen2.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen1.Location = new System.Drawing.Point(44, 49);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(182, 42);
+            this.btnOpen1.TabIndex = 9;
+            this.btnOpen1.Text = "Mahjongg";
+            this.btnOpen1.UseVisualStyleBackColor = false;
+            // 
+            // fSub6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 313);
+            this.Controls.Add(this.btnOpen8);
+            this.Controls.Add(this.btnOpen7);
+            this.Controls.Add(this.btnOpen6);
+            this.Controls.Add(this.btnOpen5);
+            this.Controls.Add(this.btnOpen4);
+            this.Controls.Add(this.btnOpen3);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnOpen1);
+            this.Name = "fSub6";
+            this.Text = "RMND";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen8;
+        private System.Windows.Forms.Button btnOpen7;
+        private System.Windows.Forms.Button btnOpen6;
+        private System.Windows.Forms.Button btnOpen5;
+        private System.Windows.Forms.Button btnOpen4;
+        private System.Windows.Forms.Button btnOpen3;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.Windows.Forms.Button btnOpen1;
+    }
+}
